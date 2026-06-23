d70a91a7f0c4d0b385b1567cb00b93797af74baa
Denis Roussel <denis.roussel@acsone.eu>
2026-06-23 14:55:40 +0200

    [18.0][ADD] website_sale_product_packaging_access

---
+++ website_sale_product_packaging_access/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Sale Product Packaging Access",
+    "summary": """This module allows to define access rights for product packagings
+    and serves as base module for others.""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainers": ["rousseldenis"],
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "depends": ["product"],
+    "data": ["security/security.xml"],
+}
